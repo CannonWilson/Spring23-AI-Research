dba8af06c58326eb034b79f0db29b493fbec5bbb
CannonWilson <CannonGWilson@gmail.com>
2023-04-10 15:05:10 -0500

    Added pylint

--- top_k_results.py
+++ top_k_results.py
@@ -1,15 +1,12 @@
-from age_model import CustomAgeNetwork
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 import torch
 import clip
-import os
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 from PIL import Image
 from sklearn import svm
-import csv
+from age_model import CustomAgeNetwork
 
 print('Initializing Models')
 
@@ -32,19 +29,17 @@ MODES = ["old", "young"]
 
 # CLIP
 EMBEDDING_DIM = 512
-device = "cuda" if torch.cuda.is_available() else "cpu"
-clip_model, clip_preprocess = clip.load("ViT-B/32", device=device)
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+clip_model, clip_preprocess = clip.load("ViT-B/32", device=DEVICE)
 
 for mode in MODES:
 
-    ds_values = [] #torch.empty(IMGS_PER_CLASS).to(device)
-    confidences = [] #torch.empty(IMGS_PER_CLASS).to(device)
-    sexes = [] #torch.empty(IMGS_PER_CLASS, dtype=torch.int8).to(device)
-    smiles = [] #torch.empty(IMGS_PER_CLASS, dtype=torch.int8).to(device)
-    correctness = [] #torch.empty(IMGS_PER_CLASS, dtype=torch.int8).to(device)
-    img_feature_stack = [] #torch.empty((IMGS_PER_CLASS, EMBEDDING_DIM)).to(device)
-
-    cur_idx = 0
+    ds_values = []
+    confidences = []
+    sexes = []
+    smiles = []
+    correctness = []
+    img_feature_stack = []
 
     print(f'Finding CLIP embeddings and attributes for {mode.upper()} images')
     with torch.no_grad():
@@ -58,7 +53,7 @@ for mode in MODES:
             img_class = path.split('/')[2]
             if not mode == img_class:
                 continue
-    
+
             # Calculate correctness/confidence of custom model
             model_output = custom_model(image)
             confidence, predicted = torch.max(model_output.data, 1)
@@ -69,29 +64,22 @@ for mode in MODES:
             if pred == actual:
                 # correctness[cur_idx] = 1
                 correctness.append(1)
-            else: 
+            else:
                 # correctness[cur_idx] = -1
                 correctness.append(-1)
 
             # Record sex/smiling of current img
             sex = path.split('/')[-3]
             smiling = path.split('/')[-2]
-            cur_sex = 1 if sex == "male" else 0
-            cur_smi = 1 if smiling == "smile" else 0
-            sexes.append(cur_sex)
-            smiles.append(cur_smi)
-            # sexes[cur_idx] = 1 if sex == "male" else 0 # 0 values allow total (male) count to be found with a simple sum
-            # smiles[cur_idx] = 1 if smiling == "smile" else 0       
+            sexes.append(1 if sex == "male" else 0)
+            smiles.append(1 if smiling == "smile" else 0)
 
             # Generate CLIP embedding
             pil_image = Image.open(path)
-            clip_img = clip_preprocess(pil_image).unsqueeze(0).to(device)
+            clip_img = clip_preprocess(pil_image).unsqueeze(0).to(DEVICE)
             img_features = clip_model.encode_image(clip_img)
             img_feature_stack.append(img_features)
 
-            # Increase index of imgs for current class
-            cur_idx += 1
-    
     print('Finished getting clip embeddings and correctness scores.')
     print('Beginning to fit SVM classifiers')
     # Fit the SVM on all of the embeddings
@@ -103,43 +91,51 @@ for mode in MODES:
             img_feature_stack[idx].numpy().transpose()) + \
                 svm_classifier.intercept_
         ds_values.append(score[0])
-    
+
     print('Fitted SVM, now plotting results.')
-    conf_sorted_idxs, ds_sorted_idxs = np.argsort(confidences), np.argsort(ds_values)
-    conf_sorted_sexes, ds_sorted_sexes = np.array(sexes)[conf_sorted_idxs], np.array(sexes)[ds_sorted_idxs]
-    conf_sorted_smiles, ds_sorted_smiles = np.array(smiles)[conf_sorted_idxs], np.array(smiles)[ds_sorted_idxs]
-    conf_sorted_frac_male, ds_sorted_frac_male = np.empty(IMGS_THIS_CLASS), np.empty(IMGS_THIS_CLASS)
-    conf_sorted_frac_smiles, ds_sorted_frac_smiles = np.empty(IMGS_THIS_CLASS), np.empty(IMGS_THIS_CLASS)
-    
+    conf_sorted_idxs =  np.argsort(confidences)
+    ds_sorted_idxs = np.argsort(ds_values)
+    conf_sorted_sexes = np.array(sexes)[conf_sorted_idxs]
+    ds_sorted_sexes = np.array(sexes)[ds_sorted_idxs]
+    conf_sorted_smiles = np.array(smiles)[conf_sorted_idxs]
+    ds_sorted_smiles = np.array(smiles)[ds_sorted_idxs]
+    conf_sorted_frac_male = np.empty(IMGS_THIS_CLASS)
+    ds_sorted_frac_male = np.empty(IMGS_THIS_CLASS)
+    conf_sorted_frac_smiles = np.empty(IMGS_THIS_CLASS)
+    ds_sorted_frac_smiles = np.empty(IMGS_THIS_CLASS)
+
     for num_people in range(IMGS_THIS_CLASS):
-        conf_num_males, ds_num_males = conf_sorted_sexes[:num_people].sum(), ds_sorted_sexes[:num_people].sum()
-        conf_num_smiles, ds_num_smiles = conf_sorted_smiles[:num_people].sum(), ds_sorted_smiles[:num_people].sum()
+        conf_num_males = conf_sorted_sexes[:num_people].sum()
+        ds_num_males = ds_sorted_sexes[:num_people].sum()
+        conf_num_smiles = conf_sorted_smiles[:num_people].sum()
+        ds_num_smiles = ds_sorted_smiles[:num_people].sum()
         conf_sorted_frac_male[num_people] = conf_num_males / num_people
         ds_sorted_frac_male[num_people] = ds_num_males / num_people
         conf_sorted_frac_smiles[num_people] = conf_num_smiles / num_people
         ds_sorted_frac_smiles[num_people] = ds_num_smiles / num_people
-    
-    assert conf_num_males == ds_num_males # make sure that the final tally for the number of males is the same
-    assert conf_num_smiles == ds_num_smiles # make sure that the final tally for the number of smiles is the same
+
+    # make sure that the final tally for the number of males is the same
+    assert conf_num_males == ds_num_males 
+    assert conf_num_smiles == ds_num_smiles
 
     if mode == "old":
-        minority_sex = "Female"
+        minority_sex = "Female" # pylint: disable=invalid-name
         sex_y_conf = 1-conf_sorted_frac_male
         sex_y_ds = 1-ds_sorted_frac_male
         sex_baseline = 1 - (conf_num_males / IMGS_THIS_CLASS)
 
-        minority_smile = "Smiling"
+        minority_smile = "Smiling" # pylint: disable=invalid-name
         smi_y_conf = conf_sorted_frac_smiles
         smi_y_ds = ds_sorted_frac_smiles
         smi_baseline = conf_num_smiles / IMGS_THIS_CLASS
-        
+
     elif mode == "young":
-        minority_sex = "Male"
+        minority_sex = "Male" # pylint: disable=invalid-name
         sex_y_conf = conf_sorted_frac_male
         sex_y_ds = ds_sorted_frac_male
         sex_baseline = conf_num_males / IMGS_THIS_CLASS
 
-        minority_smile = "Not Smiling"
+        minority_smile = "Not Smiling" # pylint: disable=invalid-name
         smi_y_conf = 1-conf_sorted_frac_smiles
         smi_y_ds = 1-ds_sorted_frac_smiles
         smi_baseline = 1 - (conf_num_smiles / IMGS_THIS_CLASS)
