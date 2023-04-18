2934e839345caf1795bd9aa28e2aa7dcd9dcf2a8
CannonWilson <CannonGWilson@gmail.com>
2023-04-18 10:36:22 -0500

    Debugging train_resnet

--- train_resnet.py
+++ train_resnet.py
@@ -74,8 +74,7 @@ bce_loss_unreduced = nn.BCEWithLogitsLoss(reduction='none')
 for epoch in range(EPOCHS):
     for idx, (images, labels) in enumerate(train_loader):
         optimizer.zero_grad(set_to_none=True)
-        images.to(DEVICE)
-        labels.to(DEVICE)
+        images = images.to(DEVICE)
         with autocast():
             logits = model(images).squeeze()
             temp_loss = bce_loss_unreduced(logits, labels.float())
